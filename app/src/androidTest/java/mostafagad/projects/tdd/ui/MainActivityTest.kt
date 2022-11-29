4202951eb612dfaa930bf5b4d98c916f39d01990
MostafaGad1911 <mostafa.gad@kashat.com.eg>
2022-11-29 17:20:31 +0200

    replace text .

--- app/src/androidTest/java/mostafagad/projects/tdd/ui/MainActivityTest.kt
+++ app/src/androidTest/java/mostafagad/projects/tdd/ui/MainActivityTest.kt
@@ -1,22 +1,29 @@
 package mostafagad.projects.tdd.ui
 
+import android.view.View
+import android.widget.EditText
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.TypeTextAction
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.google.android.material.textfield.TextInputEditText
 import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Test
 import mostafagad.projects.tdd.R
 import mostafagad.projects.tdd.utils.LoginValidator
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.endsWith
+import org.hamcrest.Matcher
 import org.junit.Assert
 import org.junit.Rule
 import org.junit.runner.RunWith
@@ -31,12 +38,20 @@ class MainActivityTest {
 
     @Test
     fun testLoginUser(){
-        onView(withId(R.id.mobileNumberEdt)).perform(ViewActions.typeText("0155317310"))
-        onView(withId(R.id.passwordNumberEdt)).perform(ViewActions.typeText("Mos55"))
+        onView(withId(R.id.mobileNumberEdt)).perform(click(), replaceText("0155317310"))
+        onView(withId(R.id.passwordNumberEdt)).perform(click(), replaceText("Mostafa15#"))
+
         val checkPhoneValid = LoginValidator.validatePhone(phone =  onView(withId(R.id.mobileNumberEdt)).toString().trim())
         val checkPasswordValid = LoginValidator.validatePassword(password = onView(withId(R.id.passwordNumberEdt)).toString().trim())
-        Espresso.closeSoftKeyboard()
         onView(withId(R.id.loginBtn)).perform(click())
         assertTrue(checkPhoneValid && checkPasswordValid)
     }
+
+    fun isEditTextInLayout(parentViewId: Int): Matcher<View> {
+        return allOf(
+            isDescendantOfA(withId(parentViewId)),
+            withClassName(endsWith("EditText"))
+        )
+    }
+
 }
